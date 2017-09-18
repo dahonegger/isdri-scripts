dae34cb7b67c8a53ccb7116368811854821479c8
simpsale <simpsale@oregonstate.edu>
2017-09-17 22:33:04 -0700

    codes and functions required to make enviro .pngs for the Guadalupe site

---
+++ cube2png/make_PNGs_with_enviro_info/batch_cube2png_guadalupe.m
@@ -0,0 +1,79 @@
+% batch file to make enviro PNGs. 
+% updated 9/17/2017 for ISDRI Guadalupe data
+
+
+%% USER INPUTS
+% add paths to ISDRI HUB Support Data and GitHub Repository
+
+% SUPPORT DATA PATH
+supportDataPath = 'D:\Data\ISDRI\SupportData'; % LENOVO HARD DRIVE
+% supportDataPath = 'E:\SupportData'; %CTR HUB 
+
+% GITHUB DATA PATH
+addpath(genpath('C:\Data\ISDRI\isdri-scripts')) %GITHUB REPOSITORY
+
+% MAT FILES LOCATION
+baseDir = 'E:\guadalupe\processed\'; %CTR HUB
+
+% PNG LOCATION
+% saveDir = 'E:\PNGs\timex_enviroInfo5\'; % CTR HUB
+saveDir = 'C:\Data\ISDRI\postprocessed\timex_enviroInfo\'; % LENOVO HARD DRIVE
+
+% rewrite existing files in save directory? true=yes
+doOverwrite = false;
+
+% Download new support data files?
+downloadWind = true;
+
+%% Prep files
+% make save directory
+addpath(genpath(supportDataPath)) 
+
+if ~exist(saveDir);mkdir(saveDir);end
+dayFolder = dir([baseDir,'2017*']);
+
+% download environmental files
+% WIND: buoy number, save directory, save fname
+if downloadWind;fetchWindNDBC(46011,fullfile(supportDataPath,'Wind'),'MetData_NDBC46011.txt'); end 
+
+%% Process Files 
+imgId = 1;
+for iDay = 1:length(dayFolder)
+        
+    dayFolder(iDay).polRun = dir(fullfile(baseDir,dayFolder(iDay).name,'*_pol.mat'));
+    saveDirSub = [saveDir,dayFolder(iDay).name];
+    if ~exist(saveDirSub);mkdir(saveDirSub);end
+    
+        for iRun = 1:length(dayFolder(iDay).polRun)
+            
+            fprintf('%3.f of %3.f in dir %3.f of %3.f: ',...
+                iRun,length(dayFolder(iDay).polRun),...
+                iDay,length(dayFolder))
+            
+            cubeName = fullfile(baseDir,dayFolder(iDay).name,dayFolder(iDay).polRun(iRun).name);
+            
+            [~,cubeBaseName,~] = fileparts(cubeName);
+            
+            pngBaseName = sprintf('%s_timex.png',cubeBaseName);
+            pngName = fullfile(saveDirSub,pngBaseName);
+            
+            fileExists = exist(pngName,'file');
+            if fileExists && ~doOverwrite
+                fprintf('%s exists. Skipping ...\n',pngName)
+            else
+                fprintf('%s ...',cubeBaseName)
+%                 try
+                    cube2png_guadalupe(cubeName,pngName)
+                    fprintf('Done.\n')
+%                 catch
+%                     fid = fopen(['FAILED_on_file_',pngBaseName,'.txt'], 'wt' );
+%                     fclose(fid)
+%                 end
+                    
+            end
+            
+            imgId = imgId + 1;
+          end
+       
+end
+      
---
+++ getEnviroInfo/loadWavesNDBC.m
@@ -0,0 +1,40 @@
+function [dnWaves,Hs,dirWaves] = loadWavesNDBC(fname)
+%loadWindNDBC loads and reads .txt file of NDBC met data
+%   file e.g. http://www.ndbc.noaa.gov/data/realtime2/44039.spec
+
+%  input variables: 
+    %   fname: file name (possibly 'WaveData_NDBC44039.txt')
+     
+%  output variables:
+    %   dnWaves: datenum time (UTC)
+    %   HsWaves: significant wave height (m)
+    %   dirWaves: direction of waves (cardinal degrees wind is coming from)
+
+% Alex Simpson 6/17/17
+% Adapted for wave data by Annika O'Dea 9/17/2107
+
+fid=fopen(fname);
+for i = 1:30
+    [~] = fgetl(fid);
+end
+tline = fgetl(fid);
+k=1;
+while ~feof(fid)   
+ 
+    tmp = textscan(tline,'%4f %02d %02d %02d %02d %f %f %f %f %f %s %s %s %f %f');
+    dnWaves(k) = datenum(double([(cell2mat(tmp(1))) cell2mat(tmp(2)) cell2mat(tmp(3))...
+       cell2mat(tmp(4)) cell2mat(tmp(5)) 0])); %UTC
+    Hs(k) = double(cell2mat(tmp(6)));
+    if ~isempty(double(cell2mat(tmp(15))))
+        dirWaves(k) = double(cell2mat(tmp(15)));
+    else 
+        dirWaves(k) = nan;
+    end
+    tline = fgetl(fid);
+    k=k+1;
+end
+
+fclose(fid);
+
+
+
